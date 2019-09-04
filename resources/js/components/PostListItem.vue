50f9c881a07383c6780f5278bcc6da96e13bde94
remco1008 <remco1008@gmail.com>
2019-09-04 20:41:47 +0200

    Formatting

--- resources/js/components/PostListItem.vue
+++ resources/js/components/PostListItem.vue
@@ -1,48 +1,58 @@
 <template>
-    <div class="box" @mouseover="hover = true" @mouseleave="hover = false" :class="{'has-background-primary has-text-white': hover}">
-        <div class="level">
-          <div class="level-left">
-            <div class="level-item">
-              <h5 class="title is-5" :class="{'has-text-white':hover}">{{ post.title }}</h5>
-            </div>
-          </div>
-
-          <div class="level-right">
-            <div class="level-item" v-if='post.rating'>
-                <star-rating :increment=0.25 :read-only='true' :rating='post.rating' :star-size=40></star-rating>
-            </div>
-          </div>
+  <div class="box"
+       @mouseover="hover = true"
+       @mouseleave="hover = false"
+       :class="{'has-background-primary has-text-white': hover}">
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <h5 class="title is-5"
+              :class="{'has-text-white':hover}">{{ post.title }}</h5>
         </div>
+      </div>
 
-  <article class="media">
-     <figure class="media-left" style='width:200px;'>
-    <p class="image is-4by5" v-html="post.image_tag">
-      {{ post.image_tag }}
-    </p>
-  </figure>
-  <div class="media-content">
-    <div class="content">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-      </p>
+      <div class="level-right">
+        <div class="level-item"
+             v-if='post.rating'>
+          <star-rating :increment=0.25
+                       :read-only='true'
+                       :rating='post.rating'
+                       :star-size=40></star-rating>
+        </div>
+      </div>
     </div>
-    </div>
-  <div class="media-right text-at-bottom">
-    <p>{{ post.display_published }}</p>
+
+    <article class="media">
+      <figure class="media-left"
+              style='width:200px;'>
+        <p class="image is-4by5"
+           v-html="post.image_tag">
+          {{ post.image_tag }}
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+          </p>
+        </div>
+      </div>
+      <div class="media-right text-at-bottom">
+        <p>{{ post.display_published }}</p>
+      </div>
+
+    </article>
   </div>
-
-</article>
- </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        hover:false,
+        hover: false
       };
     },
     props: {
-      'post': Object
+      post: Object
     }
   };
 </script>
