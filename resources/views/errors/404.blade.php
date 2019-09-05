314df28357417508d847dc3b87bacc9b39d60d0f
remco1008 <remco1008@gmail.com>
2019-09-05 12:30:35 +0200

    Basic error page for invalid urls

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends ('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+    <h1 class="title">
+        Oeps!
+    </h1>
+    <h2 class="subtitle">
+        Sorry, maar deze pagina bestaat niet (meer)
+    </h2>
+</div>
+
+@endsection
